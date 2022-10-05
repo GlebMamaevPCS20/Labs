a16fb9bd5c9bb3bf3a17a9d12b6a86be068da3a2
Gleb <glebmamaev90485@gmail.com>
2022-10-05 20:48:28 +0300

    Labs3-4 completed

---
+++ lab1-2/Lab3/Program.cs
@@ -0,0 +1,97 @@
+﻿using System.ComponentModel.Design;
+using System.Diagnostics.CodeAnalysis;
+using System.Net.Sockets;
+
+
+namespace Lab3
+{
+    class Lab3
+    {
+        static void Main()
+        {
+            Console.WriteLine("Задание 1");
+            int i = 0;
+
+            Console.WriteLine("Счетчик");
+
+            for (i = 0; i <= 100; i++)
+            {
+                if (i % 10 == 0)
+                {
+                    Console.WriteLine(i);
+                }
+            }
+
+            Console.WriteLine();
+
+            Console.WriteLine("Цикл с предусловием");
+
+            i = 0;
+            while (i <= 100)
+            {
+                if (i % 10 == 0)
+                {
+                    Console.WriteLine(i);
+                }
+                i++;
+            }
+
+            Console.WriteLine();
+
+            Console.WriteLine("Цикл с постусловием");
+            i = 0;
+            do
+            {
+                if (i % 10 == 0)
+                {
+                    Console.WriteLine(i);
+                }
+                i++;
+            }
+            while (i <= 100);
+
+            Console.WriteLine();
+
+            Console.WriteLine("Задание 2");
+            i = 0;
+            int summ = 0;
+            for (i = 0; i <= 30; i++)
+            {
+                if (i % 2 == 0)
+                {
+                    summ += i;
+
+                }
+                Console.WriteLine(summ);
+            }
+
+            Console.WriteLine();
+
+            Console.WriteLine("Задание повышенной сложности 1");
+            devRekurs(0,100,10);
+
+            Console.WriteLine();
+
+            Console.WriteLine("Задание повышенной сложности 2");
+            string str = Console.ReadLine();
+            foreach(var item in str)
+            {
+                Console.WriteLine(item);
+            }    
+        }
+        static void devRekurs(int index, int end, int dev)
+        {
+            if (index <= end)
+            {
+                if (index % dev == 0)
+                {
+                    Console.WriteLine(index);
+                }
+                devRekurs(++index, end, dev);
+            }
+
+        }
+    }
+}
+
+
---
+++ lab1-2/Lab4/Program.cs
@@ -0,0 +1,72 @@
+﻿namespace Lab4
+{
+    class Lab4
+    {
+        static void Main()
+        {
+            Console.WriteLine("Задание 1");
+            int n = 5;
+            int[] mass = new int[n];
+            Random rnd = new Random();
+            for (int i = 0; i < mass.Length; i++)
+            {
+                mass[i] = rnd.Next(0, 101);
+                Console.Write(mass[i] + "\t");
+            }
+
+            Console.WriteLine();
+
+            Console.WriteLine("Задание 2");
+            string str = Console.ReadLine();
+            string[] strArray = str.Split(' ');
+            foreach (var item in strArray)
+            {
+                Console.WriteLine(item);
+            }
+
+            Console.WriteLine();
+
+            Console.WriteLine("Задание 3");  //Сложность О(n^2), так как для каждой n-ой итерации внешнего цикла выполняется n итераций внутреннего 
+            int buffer = 0;
+            for (int i = 0; i < mass.Length; i++)
+            {
+                for (int j = i + 1; j < mass.Length; j++)
+                {
+                    if (mass[i] > mass[j])
+                    {
+                        buffer = mass[i];
+                        mass[i] = mass[j];
+                        mass[j] = buffer;
+                    }
+                }
+                Console.Write(mass[i] + "\t");
+            }
+
+            Console.WriteLine();
+
+            Console.WriteLine("Задание повышенной сложности");
+            string str1 = "Привет, как дела? Привет, Все хорошо!";
+            string word = "";
+            List<string> wordsList = new List<string>();
+            foreach (char item in str1)
+            {
+                if (item == ' ' || item == '!' || item == '?' || item == ',' || item == ':' || item == ';' || item == '.')
+                {
+                    wordsList.Add(word);
+                    word = "";
+                }
+
+                else
+                {
+                    word+=item;
+                }
+            }
+
+            foreach (string item in wordsList)
+            {
+                Console.WriteLine(item);
+            }
+
+        }
+    }
+}
